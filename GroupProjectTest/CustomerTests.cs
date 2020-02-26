8353c4da8c2f0909d48aecb0b5693ec68efd451b
kayrim <kbldn@live.co.uk>
2020-02-26 16:57:44 +0000

    Added one method and one test case using Where and parameters

---
+++ GroupProjectTest/CustomerTests.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using GroupProject01;
+
+namespace GroupProjectTests
+{
+    class CustomerTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [TestCase("mexico", "owner", 3)]
+        public void Test1MexicoOwners(string country, string contactTitle, int expected)
+        {
+            var instance = new CustomerTesting();
+
+            var actual = instance.ContactTitleInCountry(country, contactTitle);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
