0796856cb334b455116550bf1f69cb66f65832ef
jwKav <jkav201@gmail.com>
2020-02-26 17:07:11 +0000

    added NUNIT test app

---
+++ GroupProjectTests/ProductTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GroupProject01;
+using NUnit;
+using NUnit.Framework;
+
+namespace GroupProjectTests
+{
+    class ProductTests
+    {
+        [TestCase("desc", 263.5000)]
+        [TestCase("asc", 2.5000)]
+        public void UnitPriceDesc(string order, decimal expected)
+        {
+            var instance01 = new ProductTesting();
+
+            var actual = instance01.UnitPriceDesc(order);
+            //Console.WriteLine(actual);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase(true, 8)]
+        [TestCase(false, 69)]
+        public void DiscontinuedCountTest(bool IsDiscontinued, int expected)
+        {
+            var instance01 = new ProductTesting();
+
+            var actual = instance01.DiscontinuedCount(IsDiscontinued);
+
+            Assert.AreEqual(expected, actual);
+        }
+        [TestCase("bottle", 12)]
+        [TestCase("jar", 8)]
+        public void ProductsStoredIn(string container, int expected)
+        {
+            var instance01 = new ProductTesting();
+
+            var actual = instance01.ProductsStoredIn(container);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
