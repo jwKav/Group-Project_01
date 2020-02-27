ebf9231d05f165b87c63a1b20aa44622b820b051
Hassan Abdullahi <Hassan931221@gmail.com>
2020-02-27 11:31:55 +0000

    Orders Tests

--- GroupProject01/NorthwindTestMethods.cs
+++ GroupProject01/NorthwindTestMethods.cs
@@ -74,41 +74,161 @@ namespace GroupProject01
 
         }
 
-        
-        public int NumeberOfOrdersBySpecifiedCountry(string x)
-        {
 
+        public int NumeberOfOrdersBySpecifiedCountry(string country)
+        {
+            OrderList.Clear();
+            string inputCountry = country;
+            using (var connection = new SqlConnection(@"Data Source=(localdb)\mssqllocaldb; Initial Catalog = Northwind"))
+            {
+                connection.Open();
+
+                var SqlQuery = "SELECT COUNT(OrderID)"
+                                + " FROM ORDERS"
+                                + " WHERE ShipCountry = @" + country
+                                + " GROUP BY ShipCountry";
+
+                using (var command = new SqlCommand(SqlQuery, connection))
+                {
+                    SqlParameter param1 = new SqlParameter();
+                    param1.ParameterName = "@" + country;
+                    param1.Value = inputCountry;
+                    command.Parameters.Add(param1);
+                    Int32 count = (Int32)command.ExecuteScalar();
+
+                    return count;
+                    
+                }
+            }
+        }
+
+        public int NumberOfOrdersWithNoShipRegion()
+        {
             OrderList.Clear();
 
             using (var connection = new SqlConnection(@"Data Source=(localdb)\mssqllocaldb; Initial Catalog = Northwind"))
             {
                 connection.Open();
-                var SqlQuery = "SELECT ShipCountry, COUNT(OrderID)"
-                                + " FROM ORDERS"
-                                + $" WHERE ShipCountry = {x}"
-                                + " GROUP BY ShipCountry";
+
+                var SqlQuery = "SELECT OrderID, ShipRegion FROM ORDERS"
+                                + " WHERE ShipRegion is NULL";
 
                 using (var command = new SqlCommand(SqlQuery, connection))
                 {
                     var sqlReader = command.ExecuteReader();
 
-                    string columnname = "ShipCountry";
 
                     while (sqlReader.Read())
                     {
-                        string ShipCountry = sqlReader[columnname].ToString();
+                        int OrderID = Convert.ToInt32(sqlReader["OrderID"]);
 
                         var Order = new Order()
                         {
-                            ShipCountry = ShipCountry
+                            OrderID = OrderID
                         };
 
                         OrderList.Add(Order);
                     }
+
                     return OrderList.Count;
                 }
             }
         }
+
+
+        public int EmployeeWithMostOrders()
+        {
+            OrderList.Clear();
+
+            using (var connection = new SqlConnection(@"Data Source=(localdb)\mssqllocaldb; Initial Catalog = Northwind"))
+            {
+                connection.Open();
+
+                var SqlQuery = "SELECT TOP 1 EmployeeID, COUNT(OrderID) FROM Orders" +
+                                 " GROUP BY EmployeeID" +
+                                    " ORDER BY COUNT(OrderID) DESC";
+
+
+                using (var command = new SqlCommand(SqlQuery, connection))
+                {
+                    var sqlReader = command.ExecuteReader();
+
+                    while (sqlReader.Read())
+                    {
+                        int EmployeeID = Convert.ToInt32(sqlReader["EmployeeID"]);
+
+                        var Order = new Order()
+                        {
+                            EmployeeID = EmployeeID
+                        };
+
+                        OrderList.Add(Order);
+                    }
+
+                    return OrderList[0].EmployeeID;
+                }
+            }
+        }
+
+        public int NumberOfCustomerIDsWithTheSameShipName(string shipname)
+        {
+            OrderList.Clear();
+            string inputShipName = shipname;
+            using (var connection = new SqlConnection(@"Data Source=(localdb)\mssqllocaldb; Initial Catalog = Northwind"))
+            {
+                connection.Open();
+
+                var SqlQuery = "SELECT COUNT(CustomerID), ShipName"
+                                + " FROM ORDERS"
+                                + " GROUP BY ShipName" +
+                                " HAVING ShipName LIKE @shipname";
+                               
+                using (var command = new SqlCommand(SqlQuery, connection))
+                {
+                    SqlParameter param1 = new SqlParameter();
+                    param1.ParameterName = "@shipname";
+                    param1.Value = "%"+inputShipName+"%";
+                    command.Parameters.Add(param1);
+                    Int32 count = (Int32)command.ExecuteScalar();
+
+                    return count;
+
+                }
+            }
+
+        }
+
+       
+
+        public int NumberOfOrderIDWithTheSameDate(string date)
+        {
+            OrderList.Clear();
+            using (var connection = new SqlConnection(@"Data Source=(localdb)\mssqllocaldb; Initial Catalog = Northwind"))
+            {
+                connection.Open();
+
+                var sqlQuery = "SELECT COUNT(OrderID) FROM Orders" +
+                                " GROUP BY OrderDate" +
+                                " HAVING OrderDate = @date";
+
+
+                using (var command = new SqlCommand(sqlQuery, connection))
+                {
+                    SqlParameter sqlParameter = new SqlParameter();
+                    sqlParameter.ParameterName = "@date";
+                    sqlParameter.Value = date;
+                    command.Parameters.Add(sqlParameter);
+                    var count = Convert.ToInt32(command.ExecuteScalar());                    
+                    return count;
+                }
+
+            }
+        }
+
+
+
+
+
 
 
 
--- NorthwindNUnitOrdersTest/UnitTest1.cs
+++ NorthwindNUnitOrdersTest/UnitTest1.cs
@@ -8,7 +8,7 @@ namespace NorthwindNUnitOrdersTest
    
        
         [TestCase(4)]
-        public void Test1(int expected)
+        public void Test1EmployeesWithMostCustomer(int expected)
         {
             var instance = new NorthwindTestMethods();
             var actual = instance.EmployeesWithMostCustomer();
@@ -17,22 +17,56 @@ namespace NorthwindNUnitOrdersTest
 
         [TestCase(14)]
 
-        public void Test2(int expected)
+        public void Test2FreightAmountGreaterThan100(int expected)
         {
             var instance = new NorthwindTestMethods();
             var actual = instance.FreightAmountGreaterThan100();
             Assert.AreEqual(actual, expected);
         }
 
-       [TestCase("UK", 56)]
-       [TestCase("USA",122)]
-       [TestCase("France",77)]
-       [TestCase("Italy",28)]
-       public void Test3(string x, int expected)
-       {
+        [TestCase("UK", 56)]
+        [TestCase("USA", 122)]
+        [TestCase("France", 77)]
+        [TestCase("Italy", 28)]
+        public void Test3NumeberOfOrdersBySpecifiedCountry(string x, int expected)
+        {
             var instance = new NorthwindTestMethods();
             var actual = instance.NumeberOfOrdersBySpecifiedCountry(x);
             Assert.AreEqual(actual, expected);
-       }
+        }
+
+
+        [TestCase(507)]
+        public void Test4NumberOfOrdersWithNoShipRegion(int expected)
+        {
+            var instance = new NorthwindTestMethods();
+            var actual = instance.NumberOfOrdersWithNoShipRegion();
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase(4)]
+        public void Test5EmployeeWithMostOrders(int expected)
+        {
+            var instance = new NorthwindTestMethods();
+            var actual = instance.EmployeeWithMostOrders();
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase("Around the horn", 13)]
+        public void Test6NumberOfCustomerIDsWithTheSameShipName(string shipname , int expected)
+        {
+            var instance = new NorthwindTestMethods();
+            var actual = instance.NumberOfCustomerIDsWithTheSameShipName(shipname);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase("1996-07-22", 1)]
+        public void Test7NumberOfOrderIDWithTheSameDate(string date, int expected)
+        {
+            var instance = new NorthwindTestMethods();
+            var actual = instance.NumberOfOrderIDWithTheSameDate(date);
+            Assert.AreEqual(actual, expected);
+        }
+
     }
 }
